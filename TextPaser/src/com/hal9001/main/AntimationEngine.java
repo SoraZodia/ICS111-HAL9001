b62b5ef71e2c0988c4f28688b5f0851b8c002087
lorinkawamura <lorink@hawaii.edu>
2014-11-21 13:44:25 -1000

    Deleted stuff in my resources

--- TextPaser/src/com/hal9001/main/AntimationEngine.java
+++ TextPaser/src/com/hal9001/main/AntimationEngine.java
@@ -30,29 +30,36 @@ public class AntimationEngine {
 		//ARRAY AND PLOYMORPHISM FOR THE WIN
 		IPaser actors[] = 
 			{
-				new edmundgh_FileParser("effects.txt", "LaunchSite.jpg", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
+				//cody's actors
 				new codywade_Actor("pizza.png","animator3.txt"),
 				new codywade_Actor("patrick.gif","animator2.txt"),
 				new codywade_Actor("earth.gif","animator.txt"),
+				
+				//keoni's actors
 				new keonimf_Actor("hwpup.png","animator.txt", 950,256),
 				new keonimf_Actor("bat.png","animator2.txt", 1800,500),
 				new keonimf_Actor("ghostpv1.png","animator3.txt", 10,50),
-				new lorink_TestAnim("Patrick.png", "control.txt"),
-				new lorink_TestAnim("Patrick.png", "control2.txt"),
-				new lorink_TestAnim("Patrick.png", "control3.txt"),
+				
+				//lorin's actors
+				new lorink_TestAnim("asteroid1-pic.png", "control.txt"),
+				new lorink_TestAnim("asteroid2-pic.png", "control2.txt"),
+				new lorink_TestAnim("saturn-pic.png", "control3.txt"),
+				
+				//shaun's actors
 				new shaunnk_Animator("REDSKY.txt", "RedSky.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
 				new shaunnk_Animator("ATMOSPHERE.txt", "EarthAtmosphere.png", EZ.getWindowWidth()/2, -EZ.getWindowHeight()/2),
 				new shaunnk_Animator("STARS1.txt", "Stars1.png", EZ.getWindowWidth()/2, -EZ.getWindowHeight()),
 				new shaunnk_Animator("JEFF.txt", "Jeff_Boomhauer.png", 225, 310),
 				new shaunnk_Animator("PROPANE.txt", "propane_tank.png", 1000, 200),
 				new shaunnk_Animator("TEXAS.txt", "Texas_converted.png", 1500, 250),
-			    new edmundgh_FileParser("shuttleExtank.txt", "ShuttleExtank.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
+			    
+				//edmund's actors
+				new edmundgh_FileParser("shuttleExtank.txt", "ShuttleExtank.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
 				new edmundgh_FileParser("shuttleLiTank1.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)+140, (EZ.getWindowHeight()/2)+100),
 				new edmundgh_FileParser("shuttleLiTank2.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)-140, (EZ.getWindowHeight()/2)+100),
 				new edmundgh_FileParser("shuttleShip.txt", "ShuttleShip.png", EZ.getWindowWidth()/2, (EZ.getWindowHeight()/2)+60),
+				new edmundgh_FileParser("effects.txt", "LaunchSite.jpg", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
 			};	
-	
-		//trying to see if this works
 
 		while(running){
 			for(int x = 0; x < actors.length; x++){
