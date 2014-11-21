ded1a00126a6e2e571dbbed86b1b175fcf7a376e
Shaunnk <Shaunnk@users.noreply.github.com>
2014-11-20 22:10:05 -1000

    Updated background images

--- TextPaser/src/com/hal9001/main/AntimationEngine.java
+++ TextPaser/src/com/hal9001/main/AntimationEngine.java
@@ -31,27 +31,27 @@ public class AntimationEngine {
 		IPaser actors[] = 
 			{
 				new edmundgh_FileParser("effects.txt", "LaunchSite.jpg", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
-				new edmundgh_FileParser("shuttleExtank.txt", "ShuttleExtank.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
-				new edmundgh_FileParser("shuttleLiTank1.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)+140, (EZ.getWindowHeight()/2)+100),
-				new edmundgh_FileParser("shuttleLiTank2.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)-140, (EZ.getWindowHeight()/2)+100),
-				new edmundgh_FileParser("shuttleShip.txt", "ShuttleShip.png", EZ.getWindowWidth()/2, (EZ.getWindowHeight()/2)+60),
+				new codywade_Actor("pizza.png","animator3.txt"),
+				new codywade_Actor("patrick.gif","animator2.txt"),
+				new codywade_Actor("earth.gif","animator.txt"),
 				new keonimf_Actor("hwpup.png","animator.txt", 950,256),
 				new keonimf_Actor("bat.png","animator2.txt", 1800,500),
 				new keonimf_Actor("ghostpv1.png","animator3.txt", 10,50),
 				new lorink_TestAnim("Patrick.png", "control.txt"),
 				new lorink_TestAnim("Patrick.png", "control2.txt"),
 				new lorink_TestAnim("Patrick.png", "control3.txt"),
-				new shaunnk_Animator("HANK.txt", "Hank_Hill.png", 375, 310),
-				new shaunnk_Animator("BILL.txt", "Bill_Dauterive.png", 760, 310),
-				new shaunnk_Animator("DALE.txt", "Dale_Gribble.png", 115, 310),
+				new shaunnk_Animator("REDSKY.txt", "RedSky.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
+				new shaunnk_Animator("ATMOSPHERE.txt", "EarthAtmosphere.png", EZ.getWindowWidth()/2, -EZ.getWindowHeight()/2),
+				new shaunnk_Animator("STARS1.txt", "Stars1.png", EZ.getWindowWidth()/2, -EZ.getWindowHeight()),
 				new shaunnk_Animator("JEFF.txt", "Jeff_Boomhauer.png", 225, 310),
 				new shaunnk_Animator("PROPANE.txt", "propane_tank.png", 1000, 200),
 				new shaunnk_Animator("TEXAS.txt", "Texas_converted.png", 1500, 250),
-				new codywade_Actor("pizza.png","animator3.txt"),
-				new codywade_Actor("patrick.gif","animator2.txt"),
-				new codywade_Actor("earth.gif","animator.txt")
+			    new edmundgh_FileParser("shuttleExtank.txt", "ShuttleExtank.png", EZ.getWindowWidth()/2, EZ.getWindowHeight()/2),
+				new edmundgh_FileParser("shuttleLiTank1.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)+140, (EZ.getWindowHeight()/2)+100),
+				new edmundgh_FileParser("shuttleLiTank2.txt", "ShuttleLiTank.png", (EZ.getWindowWidth()/2)-140, (EZ.getWindowHeight()/2)+100),
+				new edmundgh_FileParser("shuttleShip.txt", "ShuttleShip.png", EZ.getWindowWidth()/2, (EZ.getWindowHeight()/2)+60),
 			};	
-		
+	
 		//trying to see if this works
 
 		while(running){
